e32a3457e06fd40a195857aad7662675d6bbb961
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2012-08-23 22:30:35 -0700

    basic skeleton

---
+++ src/clj_notes/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns clj-notes.views.welcome
+  (:require [clj-notes.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to clj-notes"]))
