d3a2eec721f12e3126b05c3d6a74ee509e55606c
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2012-12-19 13:16:10 +0200

    gitgnore & heap size

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
             :description "simple notes app - learning project"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]]
+            :jvm-opts ["-Xmx256M"]
             :main clj-notes.server)
 
